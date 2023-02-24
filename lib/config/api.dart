b40f680eefa0e1f9970fdb96295599c85a1284f3
DESKTOP-20CI5KE\PC's <mnurhuda009@gmail.com>
2023-02-24 22:00:03 +0700

    change

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,6 +1,5 @@
 class Api {
-  static const _baseUrl =
-      "http://192.168.1.5/BWA/discussapp/backend_discuss_app";
+  static const _baseUrl = "localhost/discussapp/backend_discuss_app";
   static const comments = "$_baseUrl/comments";
   static const follows = "$_baseUrl/follows";
   static const topics = "$_baseUrl/topics";
